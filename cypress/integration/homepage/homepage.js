3478cd4dc16fb3ccf6b56b140ad1a4b456732dee
Pierrick <pblons@sqli.com>
2020-09-26 16:58:23 +0200

    setup(cypress-cucumber): Moving cypress test case to cucumber feature

---
+++ cypress/integration/homepage/homepage.js
@@ -0,0 +1,13 @@
+import { Given, Then } from 'cypress-cucumber-preprocessor/steps';
+
+Given('I open the homepage', () => {
+    cy.visit('./public/index.html');
+
+});
+
+
+Then('I should see the coming soon message', () => {
+    cy.get('.centered')
+            .children('p')
+            .should('contain', 'Coming soon...');
+});
